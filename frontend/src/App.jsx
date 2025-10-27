23ce80111a11e65c957c922d40bb5f6096aebd20
Himanshu Shukla <himanshu.s@techostinger.com>
2025-10-27 18:15:33 +0530

    9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,7 @@ export default function App() {
       <Navbar
        />
       <div className="flex">
-        {/* <Sidebar /> */}
+        <Sidebar />
         <main className="flex-1 min-w-0 p-6">
           <Breadcrumb />
           <Routes>
